0c5c7a622ba4c0ee597314bb3c9f86fdd617f2fb
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-13 11:13:45 +0530

    powerof.java

---
+++ powerof.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class Poweroftwo
+{
+    public static void main(String args[])
+    {
+        
+        Scanner s = new Scanner(System.in);
+        int m = s.nextInt();
+  System.out.println(m*2);
+    }
+}
