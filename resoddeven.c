670c45dfc1c35b294a967683caa79ca6f1d66318
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-13 10:56:16 +0530

    resoddeven.c

--- resoddeven.c
+++ resoddeven.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void)
 {
-	int m,n;
+	int k,n;
 	int sum=0;
-	scanf("%d %d",&m,&n);
-	sum=m+n;
+	scanf("%d %d",&k,&n);
+	sum=k+n;
 	if(sum%2==0)
 	{
 		printf("even");
